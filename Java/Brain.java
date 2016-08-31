0df17e38b3756765d8254832f7abb7a2de0902e8
Henry Harris <henrythomasharris@gmail.com>
2016-08-30 23:08:16 -0400

    Added middle forking spot

--- Java/Brain.java
+++ Java/Brain.java
@@ -395,6 +395,66 @@ public class Brain {
 				return forkingMove;
 			}
 		}
+		
+		// TODO add middle square as a possible forking spot
+		tempState = new char[3][3];
+		for(int i = 0; i < 3; i++)
+			for(int j = 0; j < 3; j++)
+				tempState[i][j] = state[i][j];
+
+		// If middle is open, possible forking spot!
+		if(state[1][1] == '_') {
+			int[] forkingMove = new int[2];
+			int rank = 0;
+			int forks = 0;
+			// Take it
+			tempState[1][1] = computer.getPlayer();
+			// Can now win the row?
+			for(int i = 0; i < 3; i++) {
+				if(tempState[1][i] == computer.getPlayer())
+					rank++;
+				else if(tempState[1][i] == computer.getOpponent())
+					rank--;
+			}
+			if(rank == 2)
+				forks++;
+			rank = 0;
+			// Can now win the column?
+			for(int i = 0; i < 3; i++) {
+				if(tempState[i][1] == computer.getPlayer())
+					rank++;
+				else if(tempState[i][1] == computer.getOpponent())
+					rank--;
+			}
+			if(rank == 2)
+				forks++;
+			rank = 0;
+			// Can now win the diagonal?
+			for(int i = 0; i < 3; i++) {
+				if(tempState[i][i] == computer.getPlayer())
+					rank++;
+				else if(tempState[i][i] == computer.getOpponent())
+					rank--;
+			}
+			if(rank == 2)
+				forks++;
+			rank = 0;
+			// Can now win the other diagonal?
+			for(int i = 0; i < 3; i++) {
+				if(tempState[i][2 - i] == computer.getPlayer())
+					rank++;
+				else if(tempState[i][2 - i] == computer.getOpponent())
+					rank--;
+			}
+			if(rank == 2)
+				forks++;
+			
+			if(forks >= 2) {
+				forkingMove[0] = 1;
+				forkingMove[1] = 1;
+				return forkingMove;
+			}
+		}
 
 		return null;
 	}
